106f017b9dc4b7a8700affd2f202a59e7857d3c1
fasfashann <saintgloria.jkt@gmail.com>
2022-09-16 15:28:51 +0700

    first commit

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,13 @@
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
+
+function Layout({ children }) {
+  return (
+    <div className="container mx-auto max-w-5xl flex flex-col min-h-screen md:px-4 px-6">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+}
+export default Layout;
