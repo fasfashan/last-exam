7f2152d3a8bdb18cc628ae08699164fb8a09b526
fasfashann <saintgloria.jkt@gmail.com>
2022-09-19 14:33:58 +0700

    fourth testing dynamic opengraph and add about section

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,33 @@
-export default function About() {
+import Image from "next/image";
+import Seo from "../components/seo/Seo";
+function About() {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <>
+      <Seo />
+      <div className="grid items-baseline mt-20 md:grid-cols-12 grid-cols-4 gap-y-20">
+        <div className="md:col-span-7 col-span-12 space-y-4">
+          <h1 className="md:text-6xl text-4xl leading-tight font-bold">I’m Fasha Fadillah. I live in Bogor, where I design the future.</h1>
+          <p className="leading-relaxed text-slate-600 text-sm ">
+            I’ve loved making things for as long as I can remember, and wrote my first program when I was 6 years old, just two weeks after my mom brought home the brand new Macintosh LC 550 that I taught myself to type on.
+          </p>
+          <p className="leading-snug text-slate-600 text-sm ">
+            The only thing I loved more than computers as a kid was space. When I was 8, I climbed the 40-foot oak tree at the back of our yard while wearing my older sister’s motorcycle helmet, counted down from three, and jumped — hoping
+            the tree was tall enough that with just a bit of momentum I’d be able to get to orbit.
+          </p>
+          <p className="leading-snug text-slate-600 text-sm ">
+            I spent the next few summers indoors working on a rocket design, while I recovered from the multiple surgeries it took to fix my badly broken legs. It took nine iterations, but when I was 15 I sent my dad’s Blackberry into orbit
+            and was able to transmit a photo back down to our family computer from space.
+          </p>
+          <p className="leading-snug text-slate-600 text-sm ">
+            Today, I’m the founder of Planetaria, where we’re working on civilian space suits and manned shuttle kits you can assemble at home so that the next generation of kids really can make it to orbit — from the comfort of their own
+            backyards.
+          </p>
+        </div>
+        <div className="md:col-start-9 md:col-end-13 col-span-12 w-full ">
+          <Image className=" object-cover dark:bg-zinc-800" src="/photoprofile.jpg" layout="responsive" height={1032} width={932} alt="Fasha" />
+        </div>
+      </div>
+    </>
   );
 }
+export default About;
