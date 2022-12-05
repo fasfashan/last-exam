b4d35a13829a11ce1051dfc58305ad3796d56140
fasfashann <saintgloria.jkt@gmail.com>
2022-12-05 17:30:51 +0700

    add new case study, niagahoster

--- src/pages/lomba-niagahoster.js
+++ src/pages/lomba-niagahoster.js
@@ -2,20 +2,116 @@ import React from "react";
 import Niagahoster from "/public/assets/Lomba Niagahoster.webp";
 import Seo from "../components/seo/Seo";
 import Image from "next/image";
+import Artikel from "/public/assets/artikel-lomba.png";
+import { FiExternalLink, FiEdit2 } from "react-icons/fi";
+import Designproses from "/public/assets/desainproses.png";
+import Moodboard from "/public/assets/moodboard.webp";
+import Lowfidelity from "/public/assets/low-fidelity.webp";
+import Wireframe1 from "/public/assets/wireframe-1.webp";
+import Wireframe2 from "/public/assets/wireframe-2.webp";
+import Highfidelity1 from "/public/assets/High-fidelity-1.webp";
+import Highfidelity2 from "/public/assets/High-fidelity.webp";
+import Link from "next/link";
 const lombaniagahoster = () => {
   return (
     <>
       <Seo templateTitle="Lomba Landing Page" />
-      <div className="md:mt-20 mt-16 ">
+
+      <div className="md:mt-20 mt-16 md:px-20">
         <div className="grid grid-cols-12">
-          <h1 className="md:text-7xl text-primary mb-8 col-span-12  text-4xl  text-primary font-semibold leading-tight">Lomba Landing Page: Journey with Niagahoster</h1>
+          <h1 className="md:text-5xl text-primary mb-8 col-span-12  text-4xl  text-primary font-semibold leading-tight">Lomba Landing Page: Journey with Niagahoster</h1>
           <div className="col-span-12">
             <Image src={Niagahoster} alt="Landing page niagahoster" />
           </div>
         </div>
-        <div className="mt-20">
-          <h3 className="text-2xl font-semibold text-primary ">Overview</h3>
-          <p>Pertengahan Juni, ketika sedang asyik berselancar di Instagram, saya melihat postingan dari Niagahoster yang sedang mengadakan lomba membuat landing page. Lalu karena tertarik, saya mengunjungi detail artikel tersebut. </p>
+        <div className="mt-36 grid grid-cols-12 gap-8 items-center ">
+          <div className="md:col-span-6 col-span-12">
+            <h3 className="md:text-4xl text-2xl font-semibold text-primary ">Overview</h3>
+            <p className="text-lg text-secondary mt-2">
+              Pertengahan Juni, ketika sedang asyik berselancar di Instagram, saya melihat postingan dari Niagahoster yang sedang mengadakan lomba membuat landing page. Lalu karena tertarik, saya mengunjungi detail artikel tersebut.{" "}
+            </p>
+            <div className="flex items-center space-x-2 mt-2">
+              <a target="_blank" rel="noreferrer" href="https://www.niagahoster.co.id/blog/lomba-landing-page/" className="text-lg font-semibold underline text-primary">
+                Kunjung artikel
+              </a>
+
+              <FiExternalLink />
+            </div>
+          </div>
+          <div className="border-2 border-slate-200 md:col-span-6 col-span-12 p-4 rounded-md shadow-sm">
+            <Image src={Artikel} />
+          </div>
+        </div>
+        <div className="col-span-12 mt-8 ">
+          <p className="text-lg text-secondary">Artikel berikut berisi tentang ketentuan lomba, peraturan dan informasi-informasi lainnya. Berdasarkan artikel tersebut, setidaknya ada 3 ketentuan konten website:</p>
+          <ol type="1">
+            <li className="text-lg text-secondary mt-2">
+              1. Tema artikel mengenai “Journey kamu bersama Niagahoster”. Misalnya: Review mengenai produk dan service Niagahoster, memanfaatkan blog, ebook, forum, event Niagahoster untuk membantu kita belajar.
+            </li>
+            <li className="text-lg text-secondary mt-2">2. Tulisan tidak boleh mengandung unsur pornografi, judi, sara atau tindakan yang melanggar hukum.</li>
+            <li className="text-lg text-secondary mt-2">3. Tulisan harus original (karya sendiri), bukan plagiat ataupun copy paste.</li>
+          </ol>
+        </div>
+        <div className="mt-36">
+          <h3 className="md:text-4xl text-2xl font-semibold text-primary">Mengapa akhirnya saya mengikuti lomba ini?</h3>
+          <p className="text-lg text-secondary mt-2">
+            Tentunya selain karena hadiahnya yang menurut saya lumayan, ini adalah lomba pertama saya yang berhubungan UI Design, sekaligus menambah portfolio saya jika saya bisa memenangkan lomba ini, dan kebetulan tasks di kantor tidak
+            terlalu menumpuk😋 Tanpa pikir panjang, akhirnya saya memutuskan untuk mendaftar.
+          </p>
+        </div>
+        <div className="mt-36">
+          <h3 className="md:text-4xl text-2xl font-semibold text-primary">My design proses</h3>
+          <p className="text-lg text-secondary mt-2 mb-4">Karena ini hanya landing page yang berisi artikel, saya tidak memerlukan proses yang panjang. Kurang lebih ini proses saya dalam mendesain landing page kali ini:</p>
+          <Image src={Designproses} />
+        </div>
+        <div className="mt-36">
+          <h3 className="md:text-4xl text-2xl font-semibold text-primary">Collect moodboard</h3>
+          <p className="text-lg text-secondary mt-2 mb-4">
+            Saya memang senang memulai wireframe dari coret-coretan kertas seperti foto di bawah ini, alasannya karena terasa lebih leluasa ketimbang menggunakan tools. Saya suka kutipan dari buku Petunjuk UX milik @halodesigners
+            “High-fi memang tujuan akhir kita, namun low-fi tempatnya menemukan kejelasan (clarity) dari alur kerja yang product kita ingin capai.”
+          </p>
+
+          <Image src={Moodboard} />
+        </div>
+        <div className="mt-36">
+          <h3 className="md:text-4xl text-2xl font-semibold text-primary">Wireframe</h3>
+          <p className="text-lg text-secondary mt-2 mb-4">
+            Saya memang senang memulai wireframe dari coret-coretan kertas seperti foto di bawah ini, alasannya karena terasa lebih leluasa ketimbang menggunakan tools. Saya suka kutipan dari buku Petunjuk UX milik @halodesigners
+            “High-fi memang tujuan akhir kita, namun low-fi tempatnya menemukan kejelasan (clarity) dari alur kerja yang product kita ingin capai.”
+          </p>
+
+          <Image src={Lowfidelity} />
+          <p className="text-lg text-secondary mt-2 mb-4">Setelah coret-coret di kertas, barulah saya membuatnya menggunakan Figma.</p>
+          <div className="gap-y-4 flex flex-col">
+            <Image src={Wireframe1} />
+            <Image src={Wireframe2} />
+          </div>
+          <p className="text-lg text-secondary mt-2 mb-4">Setelah membuat wireframe, saya memiliki 5 section yang berisi konten sesuai dengan kriteria pemenang, di antaranya adalah: </p>
+          <ol type="1">
+            <li className="text-lg text-secondary mt-2">1. Kesesuaian Tema, Kualitas Konten, Ide & Kreativitas Konten</li>
+            <li className="text-lg text-secondary mt-2">2. Design website.</li>
+            <li className="text-lg text-secondary mt-2">3. Memenuhi semua syarat ketentuan yang berlaku.</li>
+          </ol>
+          <p className="text-lg text-secondary mt-2 mb-4">
+            Selain estetika desain, konten juga menjadi poin yang berpengaruh. Bagi saya ini salah satu bagian tersulit, yaitu menulis cerita, karena 90% konten berisi tulisan. Mari lanjut ke bagian paling menyenangkan, design!
+          </p>
+        </div>
+        <div className="mt-36">
+          <h3 className="md:text-4xl text-2xl font-semibold text-primary mb-4">Design High Fidelity</h3>
+
+          <div className="gap-y-4 flex flex-col">
+            <Image src={Highfidelity1} />
+            <Image src={Highfidelity2} />
+          </div>
+          <p className="text-lg text-secondary mt-2 mb-4">
+            Setelah proses desain selesai, saya akan slicing desain yang saya buat sendiri ke dalam HTML dan CSS. Website tersebut tertuang dalam domain http://fashafadillah.site namun karena saya lupa membayar domain, website tersebut
+            sudah ter-blokir. Source code dapat dilihat di Github saya
+          </p>
+          <div>
+            <a href="https://github.com/fasfashan/lomba-landing-page-niagahoster">
+              <img className="w-full" src="https://gh-card.dev/repos/fasfashan/lomba-landing-page-niagahoster.svg"></img>
+            </a>
+          </div>
         </div>
       </div>
     </>
