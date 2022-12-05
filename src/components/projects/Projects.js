4bbb6cc0b89d2990b29223f92485c23373e8cbf9
fasfashann <saintgloria.jkt@gmail.com>
2022-12-05 23:51:10 +0700

    add staycation case study

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -6,6 +6,7 @@ import "aos/dist/aos.css";
 import Image from "next/future/image";
 import Niagahoster from "/public/assets/Lomba Niagahoster.webp";
 import Prowork from "/public/assets/Prowork.webp";
+import Staycation from "/public/assets/staycationwebp.webp";
 import { FiExternalLink, FiEdit2 } from "react-icons/fi";
 import Link from "next/link";
 function Hero() {
@@ -15,7 +16,7 @@ function Hero() {
 }
 const Projects = () => {
   return (
-    <section id="projects" className="mt-60 grid grid-cols-12 gap-8  gap-y-10 ">
+    <section id="projects" className="mt-60 grid grid-cols-12 gap-8  gap-y-20 ">
       <div data-aos="fade-down" data-aos-duration="1500" className="md:col-span-6 col-span-12 ">
         <div>
           <Image src={Niagahoster} alt="Lomba Landing Page Niagahoster" className="shadow-lg " />
@@ -51,6 +52,26 @@ const Projects = () => {
           <h2 className=" text-primary text-lg  font-bold">Prowork: Coworking Space Web App</h2>
           <p className="text-secondary text-lg">
             Ruang coworking adalah ruang kerja bersama yang biasanya digunakan oleh pekerja lepas, pengusaha, pekerja jarak jauh, dan profesional independen lainnya. Website ini memudahkan mereka mencari tempat untuk bekerja.
+          </p>
+        </div>
+        <div className="flex items-center space-x-2">
+          <a className="text-lg font-semibold underline text-primary">In Progress</a>
+
+          <FiEdit2 />
+        </div>
+      </div>
+      <div data-aos="fade-up" data-aos-duration="1500" className="md:col-span-6 col-span-12 ">
+        <div>
+          <Image src={Staycation} className="shadow-lg" />
+          <div className="flex gap-4 mt-8">
+            <Tagsprogress>Design</Tagsprogress>
+            <Tagsprogress>Web Apps</Tagsprogress>
+          </div>
+        </div>
+        <div className="mt-4">
+          <h2 className=" text-primary text-lg  font-bold">Staycation Application</h2>
+          <p className="text-secondary text-lg">
+            StayCaion adalah aplikasi untuk membantu masyarakat dalam menemukan rumah, hotel ataupun apartement yang bisa dijadikan sebagai tempat berlibur. Tempat yang disediakan biasanya memiliki lokasi yang dekat dengan wisata liburan.
           </p>
         </div>
         <div className="flex items-center space-x-2">
